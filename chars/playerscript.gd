fea3ff663a5a19e80c89c3550960a415b169269f
DISN-kolo <arrtemsky@gmail.com>
2024-11-04 21:18:39 +0100

    char movement, camera movement, cursor

--- chars/playerscript.gd
+++ chars/playerscript.gd
@@ -3,23 +3,28 @@ extends CharacterBody2D
 
 const SPEED = 200
 const SLOWDOWN = 30
+const EPSILON = 1e-6
+var directionx
+var directiony
 
 
 func _physics_process(delta: float) -> void:
 
-	var directionx := Input.get_axis("LEFT", "RIGHT")
-	if directionx:
-		velocity.x = directionx * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SLOWDOWN)
-		
-	var directiony := Input.get_axis("UP", "DOWN")
-	if directiony:
-		velocity.y = directiony * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SLOWDOWN)
-	
-	if directionx or directiony:
-		velocity = velocity.normalized() * SPEED
+	directionx = Input.get_axis("LEFT", "RIGHT")
+	directiony = Input.get_axis("UP", "DOWN")
+	velocity.x = speed_calc(directionx, velocity.x)
+	velocity.y = speed_calc(directiony, velocity.y)
+
+	# avoid diagonal superspeed
+	if directionx > EPSILON or directiony > EPSILON or directionx < -EPSILON or directiony < -EPSILON:
+		if velocity.length() > SPEED:
+			velocity = velocity.normalized() * SPEED
 
 	move_and_slide()
+
+func speed_calc(dir: float, vel_dirred: float) -> float:
+	if -EPSILON > dir or dir > EPSILON:
+		vel_dirred = dir * SPEED
+	else:
+		vel_dirred = move_toward(vel_dirred, 0, SLOWDOWN)
+	return (vel_dirred)
