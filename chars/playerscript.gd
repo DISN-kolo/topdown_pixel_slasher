82ee5f538212200fe4b4bb71983d9ff9e45205d2
DISN-kolo <arrtemsky@gmail.com>
2024-11-05 23:49:19 +0100

    while attempting to standartize animations and changing sliding, an animation bug was discovered. for some reason, walking/sliding in the same direction as looking produces a quick flicker in the opposite direction right at the start and at the end of the movement

--- chars/playerscript.gd
+++ chars/playerscript.gd
@@ -7,6 +7,7 @@ const EPSILON = 1e-6
 const SLIDE_MULT = 1.8
 
 var current_top_speed : float = SPEED
+var movement_multiplier : float = 1.0
 
 var directionx : float
 var directiony : float
@@ -16,6 +17,10 @@ var is_asked_to_move : bool = false
 var is_moving : bool = false
 var is_sliding : bool = false
 
+var tween_slide : Tween
+
+var look_there : Vector2 = Vector2(0, 1)
+
 @onready var anim_tree = $AnimationTree
 @onready var cursor = $"../cursor"
 
@@ -24,50 +29,77 @@ func _physics_process(delta: float) -> void:
 	directionx = Input.get_axis("LEFT", "RIGHT")
 	directiony = Input.get_axis("UP", "DOWN")
 	direction = Vector2(directionx, directiony)
-	velocity = speed_calc(direction, velocity, delta)
 
+	if Input.is_action_just_pressed("SLIDE"):
+		if !is_sliding:
+			tween_slide = create_tween() 
+			tween_slide.tween_callback(func(): movement_multiplier=SLIDE_MULT)
+			tween_slide.tween_callback(func(): is_sliding=true)
+			tween_slide.tween_callback(func(): is_moving=true)
+			tween_slide.tween_property(self, "movement_multiplier", SLIDE_MULT*0.9, 0.5)
+			tween_slide.tween_property(self, "movement_multiplier", SLIDE_MULT*0.5, 0.02)
+			tween_slide.tween_callback(func(): is_sliding=false)
+	
+	velocity = speed_calc(direction, velocity, delta, movement_multiplier)
 	# avoid diagonal superspeed and set conditions for animations
 	if direction.length() > EPSILON:
 		is_asked_to_move = true
-		is_sliding = Input.is_action_pressed("SLIDE")
 		current_top_speed = SPEED
 		if is_sliding:
-			current_top_speed = SPEED * SLIDE_MULT
-			velocity *= SLIDE_MULT
+			current_top_speed = SPEED * movement_multiplier
 		if velocity.length() > current_top_speed:
 			velocity = velocity.normalized() * current_top_speed
 	else:
 		is_asked_to_move = false
-		is_sliding = false
+		#if !is_sliding:
+			#is_asked_to_move = false
+			#is_sliding = false
 	
 	is_moving = velocity.length() > EPSILON
-	
-	if is_moving:
-		if is_sliding:
-			anim_tree.set("parameters/conditions/is_idle", false)
-			anim_tree.set("parameters/conditions/is_moving", false)
-			anim_tree.set("parameters/conditions/is_sliding", true)
-			anim_tree.set("parameters/Slide/blend_position",
-				-velocity.normalized())
+	if is_sliding:
+		if !is_asked_to_move:
+			look_there = -(cursor.global_position - self.global_position).normalized()
 		else:
-			anim_tree.set("parameters/conditions/is_idle", false)
-			anim_tree.set("parameters/conditions/is_moving", true)
-			anim_tree.set("parameters/conditions/is_sliding", false)
-			anim_tree.set("parameters/Walk/blend_position",
-				-velocity.normalized())
+			look_there = -direction.normalized()
+		anim_tree.set("parameters/conditions/is_idle", false)
+		anim_tree.set("parameters/conditions/is_moving", false)
+		anim_tree.set("parameters/conditions/is_sliding", true)
+		pass_look_vector_to_animation(anim_tree, look_there)
+	elif is_moving:
+		#look_there = -velocity.normalized()
+		if !is_asked_to_move:
+			look_there = (cursor.global_position - self.global_position).normalized()
+		else:
+			look_there = -direction.normalized()
+		anim_tree.set("parameters/conditions/is_idle", false)
+		anim_tree.set("parameters/conditions/is_moving", true)
+		anim_tree.set("parameters/conditions/is_sliding", false)
+		pass_look_vector_to_animation(anim_tree, look_there)
 	else:
+		look_there = (cursor.global_position - self.global_position).normalized()
+		#if !is_asked_to_move:
+			#look_there = (cursor.global_position - self.global_position).normalized()
+		#else:
+			#look_there = -velocity.normalized()
 		anim_tree.set("parameters/conditions/is_moving", false)
 		anim_tree.set("parameters/conditions/is_idle", true)
 		anim_tree.set("parameters/conditions/is_sliding", false)
-		anim_tree.set("parameters/Idle/blend_position",
-			(cursor.global_position - self.global_position).normalized())
+		pass_look_vector_to_animation(anim_tree, look_there)
 	move_and_slide()
 
-func speed_calc(dir: Vector2, vel_dirred: Vector2, delta: float) -> Vector2:
+func speed_calc(dir: Vector2, vel_dirred: Vector2, delta: float, mult : float) -> Vector2:
 	if dir.length() > EPSILON:
-		vel_dirred = dir * SPEED
+		vel_dirred = dir * SPEED * mult
 	else:
 		vel_dirred.x = move_toward(vel_dirred.x, 0, SLOWDOWN * delta * 60)
 		vel_dirred.y = move_toward(vel_dirred.y, 0, SLOWDOWN * delta * 60)
 	vel_dirred *= delta * 60
 	return (vel_dirred)
+
+func pass_look_vector_to_animation(at: AnimationTree, dir: Vector2) -> void:
+	## idle animation is stupid and inverts up and down.
+	at.set("parameters/Idle/blend_position", Vector2(dir.x, -dir.y))
+	## walk animation is stupid and inverts left and right.
+	at.set("parameters/Walk/blend_position", Vector2(-dir.x, dir.y))
+	## slide animation is stupid and inverts left and right.
+	at.set("parameters/Slide/blend_position", Vector2(-dir.x, dir.y))
